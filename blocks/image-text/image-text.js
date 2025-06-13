0e6dc3702f4ef31a5dec30a0bc3b9829d6c0e80a
Snehal-Tele <snehal.tele@cognizant.com>
2025-06-13 10:45:20 +0530

    m4

--- blocks/image-text/image-text.js
+++ blocks/image-text/image-text.js
@@ -1,19 +1,19 @@
 export default function decorate(block) {
     block.classList.add('image-text-block');
-   
-    const rows = Array.from(block.children);
-   
-    rows.forEach((row, rowIndex) => {
-      const cols = Array.from(row.children);
-   
-      if (rowIndex === 0) {
-        cols[0].classList.add('image-text-title');
-      } else if (rowIndex === 1) {
-        cols[0].classList.add('image-text-content');
-        cols[1].classList.add('image-text-image');
-   
-        const img = cols[1].querySelector('picture');
-        if (img) img.classList.add('image-text-picture');
-      }
-    });
+  
+    const table = block.querySelector('table');
+    if (!table) return;
+  
+    const rows = table.rows;
+  
+    if (rows.length >= 1) rows[0].cells[0].classList.add('image-text-title');
+    if (rows.length >= 2) {
+      rows[1].cells[0].classList.add('image-text-content');
+      rows[1].cells[1].classList.add('image-text-image');
+  
+      const picture = rows[1].cells[1].querySelector('picture');
+      if (picture) picture.classList.add('image-text-picture');
+    }
+    if (rows.length >= 3) rows[2].cells[0].classList.add('image-text-link');
   }
+  
